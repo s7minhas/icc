9dda671f46aea58b1813d84a684f3ec4b4946176
s7minhas <s7.minhas@gmail.com>
2018-02-09 13:21:12 -0500

    set up infrastructure

---
+++ R/data/1_buildData.R
@@ -0,0 +1,3 @@
+if(Sys.info()['user'] %in% c('s7m', 'janus829')){
+source('~/Research/icc/R/setup.R')
+}
---
+++ R/setup.R
@@ -0,0 +1,10 @@
+rm(list=ls()
+if(Sys.info()['user'] %in% c('s7m', 'janus829')){
+pathDrop='~/Dropbox/Research/icc/'
+pathData=paste0(pathDrop, 'data')
+pathResults=paste0(pathDrop,'results')
+pathGraphics=paste0(pathGraphics,'graphics')
+}
+
+#
+
