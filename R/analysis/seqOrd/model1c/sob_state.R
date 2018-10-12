ca0f9c79a4e5da3fd923902c11a75de2b39742b2
s7minhas <s7.minhas@gmail.com>
2018-10-11 22:47:53 -0400

    copy over templates

--- R/analysis/seqOrd/model1c/sob_state.R
+++ R/analysis/seqOrd/model1c/sob_state.R
@@ -17,7 +17,7 @@ sobStateVars = c(
 	'lag1_osv_state_cumul',	
 	# p5 vars: 
 	'lag1_p5_absidealdiffMin',
-	'lag1_p5_defAllyAvg',
+	'lag1_p5_defAllyMax',
 	'lag1_p5_gov_clean', 'lag1_p5_reb_clean'
 	)
 
@@ -36,62 +36,41 @@ if(!file.exists(paste0(pathData, 'sobState_imp.rda'))){
 # pick a few from the posterior
 set.seed(6886)
 frame = data.frame(impData$Y.pmean)
-frame = cbind(data[,c('ccode','year')], frame)
-frame$icclevel_state = as.integer(frame$icclevel_state + 1)
+frame = cbind(data[,c('ccode','year','icclevel_state_3')], frame)
+frame$icclevel_state_3 = as.integer(frame$icclevel_state_3 + 1)
 frame$ccode = as.integer(frame$ccode)
 impDFs = lapply(sample(500:1000, 10), function(i){
 	x = data.frame(impData$Y.impute[,,i])
-	x = cbind(data[,c('ccode','year')], x)
+	x = cbind(data[,c('ccode','year','icclevel_state_3')], x)
 	names(x) = names(frame)
-	x$icclevel_state = as.integer(x$icclevel_state + 1)
+	x$icclevel_state_3 = as.integer(x$icclevel_state_3 + 1)
 	x$ccode = as.integer(x$ccode)
 	return(x) })
 ###############################################################
 
 ###############################################################
+# category specific effects
+sobStateVars[c(5,8,9)] = paste0('cs(',sobStateVars[c(5,8,9)],')')
+
 # pool
-if(!file.exists(paste0(pathResults, 'sobState_mod.rda'))){
-	sobStateForm = formula(
-		paste0('icclevel_state ~ ', 
-			paste(sobStateVars, collapse = ' + ') ) )
-	mod = brm(
-		formula=sobStateForm, 
-		data=frame,
-		family=cratio(link='logit')
-		)
-	save(mod, file=paste0(pathResults, 'sobState_mod.rda'))
-} else {load(paste0(pathResults, 'sobState_mod.rda'))}
+sobStateForm = formula(
+	paste0('icclevel_state_3 ~ ', 
+		paste(sobStateVars, collapse = ' + ') ) )
+mod = brm(
+	formula=sobStateForm, 
+	data=frame,
+	family=cratio(link='logit')
+	)
+save(mod, file=paste0(pathResults, 'sobState_model1a.rda'))
 
 # hier
-if(!file.exists(paste0(pathResults, 'sobState_mod_hier.rda'))){
-	sobStateForm = formula(
-		paste0('icclevel_state ~ ', 
-			paste(sobStateVars, collapse = ' + '), '+(1|ccode)' ) )
-	modHier = brm(
-		formula=sobStateForm, 
-		data=frame,
-		family=cratio(link='logit')
-		)
-	save(modHier, file=paste0(pathResults, 'sobState_mod_hier.rda'))
-} else {load(paste0(pathResults, 'sobState_mod_hier.rda'))}
-
-# summarize
-sobStateMods = lapply(list(mod, modHier), function(x){
-	summ=data.frame(fixef(x)[,1:2])
-	names(summ) = c('beta','serror')
-	summ$z = summ$beta/summ$serror
-	return(summ) })
-
-# table
-# clean table
-lazyCleanVars = gsub('_','',sobStateVars,fixed=TRUE)
-lazyCleanMods = c('m1')
-lab='$^{**}$ and $^{*}$ indicate significance at $p< 0.05 $ and $p< 0.10 $, respectively.'
-res=getTable(sobStateVars,lazyCleanVars,sobStateMods[[1]],lazyCleanMods)
-print.xtable(xtable(res, align='llcc', caption=lab),
-	include.rownames=FALSE,
-	sanitize.text.function = identity,
-	hline.after=c(0,0,length(sobStateVars)*2,length(sobStateVars)*2),
-	size="footnotesize",	
-	file=paste0(pathResults, 'sobState.tex'))
+sobStateForm = formula(
+	paste0('icclevel_state_3 ~ ', 
+		paste(sobStateVars, collapse = ' + '), '+(1|ccode)' ) )
+modHier = brm(
+	formula=sobStateForm, 
+	data=frame,
+	family=cratio(link='logit')
+	)
+save(modHier, file=paste0(pathResults, 'sobState_model1a_hier.rda'))
 ###############################################################
