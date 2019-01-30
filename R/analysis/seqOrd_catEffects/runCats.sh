5a2fbb360c9df2a5de1f60241def34960f609091
s7minhas <s7.minhas@gmail.com>
2019-01-30 11:59:35 -0500

    sh scripts to run

---
+++ R/analysis/seqOrd_catEffects/runCats.sh
@@ -0,0 +1,1 @@
+Rscript model1a_1/sob_opp.R ; Rscript model1a_1/sob_state.R  ; Rscript model1a_1/summ.R  ; Rscript model1a_2/sob_opp.R ; Rscript model1a_2/sob_state.R  ; Rscript model1a_2/summ.R ; Rscript model1a_3/sob_opp.R ; Rscript model1a_3/sob_state.R  ; Rscript model1a_3/summ.R ; Rscript model1a_4/sob_opp.R ; Rscript model1a_4/sob_state.R  ; Rscript model1a_4/summ.R  
