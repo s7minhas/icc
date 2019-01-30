3cf0583c0eec1e2f592bcd7c630decaeced40469
s7minhas <s7.minhas@gmail.com>
2019-01-30 13:02:47 -0500

    lazy parallelization

--- R/analysis/seqOrd_global/runGlobe.sh
+++ R/analysis/seqOrd_global/runGlobe.sh
@@ -1,8 +1,8 @@
-Rscript model1a_1/sob_opp.R & Rscript model1a_1/sob_state.R & Rscript model1a_2/sob_opp.R ;
+Rscript model1a_2/sob_opp.R & Rscript model1a_2/sob_state.R ;
 
 echo 'woohoo' ;
 
-Rscript model1a_2/sob_state.R & Rscript model1a_2/sob_state.R & Rscript model1a_3/sob_opp.R ;
+Rscript model1a_2/sob_state.R & Rscript model1a_3/sob_opp.R ;
 
 echo 'woohoo woohoo' ;
 
@@ -11,3 +11,17 @@ Rscript model1a_3/sob_state.R & Rscript model1a_4/sob_opp.R & Rscript model1a_4/
 echo 'woohoo woohoo woohoo' ;
 
 Rscript model1a_1/summ.R ; Rscript model1a_2/summ.R ; Rscript model1a_3/summ.R ; Rscript model1a_4/summ.R  
+
+# Rscript model1a_1/sob_opp.R & Rscript model1a_1/sob_state.R & Rscript model1a_2/sob_opp.R ;
+
+# echo 'woohoo' ;
+
+# Rscript model1a_2/sob_state.R & Rscript model1a_2/sob_state.R & Rscript model1a_3/sob_opp.R ;
+
+# echo 'woohoo woohoo' ;
+
+# Rscript model1a_3/sob_state.R & Rscript model1a_4/sob_opp.R & Rscript model1a_4/sob_state.R ; 
+
+# echo 'woohoo woohoo woohoo' ;
+
+# Rscript model1a_1/summ.R ; Rscript model1a_2/summ.R ; Rscript model1a_3/summ.R ; Rscript model1a_4/summ.R  
