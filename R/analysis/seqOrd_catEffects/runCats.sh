fd41525ca17a157e99087fcab129ab65c4e5e090
s7minhas <s7.minhas@gmail.com>
2019-02-05 08:44:54 -0500

    modify run scripts

--- R/analysis/seqOrd_catEffects/runCats.sh
+++ R/analysis/seqOrd_catEffects/runCats.sh
@@ -1,13 +1,5 @@
-Rscript model1a_1/sob_opp.R & Rscript model1a_1/sob_state.R & Rscript model1a_2/sob_opp.R ;
+Rscript model1a_1_newp5Var_pts/sob_state.R & Rscript model1a_1_newp5Var_ptsCivilWarOnly/sob_state.R & Rscript model1a_1_newp5Var_ptsCivilWarOnly/sob_opp.R ;
 
 echo 'woohoo' ;
 
-Rscript model1a_2/sob_state.R & Rscript model1a_2/sob_state.R & Rscript model1a_3/sob_opp.R ;
-
-echo 'woohoo woohoo' ;
-
-Rscript model1a_3/sob_state.R & Rscript model1a_4/sob_opp.R & Rscript model1a_4/sob_state.R ; 
-
-echo 'woohoo woohoo woohoo' ;
-
-Rscript model1a_1/summ.R ; Rscript model1a_2/summ.R ; Rscript model1a_3/summ.R ; Rscript model1a_4/summ.R  
+Rscript model1a_1_newp5Var_pts/summ.R ; Rscript model1a_1_newp5Var_ptsCivilWarOnly/summ.R
