10035ae9ec7152a5b86232dc0c4d5c02a3670ac8
s7minhas <s7.minhas@gmail.com>
2019-02-23 01:04:36 -0500

    viz weirdness ... prolly best to stick with tables

--- R/analysis/posteriorViz.R
+++ R/analysis/posteriorViz.R
@@ -127,19 +127,27 @@ ggplot(ggData, aes(x=value, y=variable)) +
 		)
 
 # marginal effects
+plot(
+	marginal_effects(
+		oppMod, 
+		effects='lag1_p5_absidealdiffMin', 
+		categorical=TRUE
+		)	
+	)
+
 load(paste0(pathData, 'mergedData_yrly_ongoing.rda.rda'))
 x = marginal_effects(
 	oppMod, 
-	effects='lag1_v2juncind', 
+	effects='lag1_p5_absidealdiffMin', 
 	categorical=TRUE)[[1]]
 
 ggplot(data=x, 
 	aes(
-		x=lag1_v2juncind, y=estimate__, 
+		x=lag1_p5_absidealdiffMin, y=estimate__, 
 		color=cats__, fill=cats__
 		)) + 
 	geom_line() + 
 	geom_ribbon(aes(ymin=lower__,ymax=upper__),alpha=.5) + 
-	geom_rug(data=data, aes(x=lag1_v2juncind,y=0),sides='b') +
+	# geom_rug(data=data, aes(x=lag1_v2juncind,y=0),sides='b') +
 	facet_wrap(~cats__, scales='free_y')
 ###############################################################
