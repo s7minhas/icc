5bcc5e23e8c833878ee07f26d916add766dedd7a
s7minhas <s7.minhas@gmail.com>
2020-01-21 20:49:38 -0500

    perf analysis

--- R/analysis/posteriorViz.R
+++ R/analysis/posteriorViz.R
@@ -10,12 +10,13 @@ if(Sys.info()['user'] %in% c('herme','Owner')){
 }
 
 #
+set.seed(6886)
 loadPkg(
 	c(
 		'brms', 'bayesplot', 'MASS',
 		# tadaa is a wrapper around ryourready
 		# https://github.com/cran/ryouready/blob/5d76b21f98737ac2778cd207b56309bcc78df9ed/R/association_measures.r
-		'tadaatoolbox',
+		'tadaatoolbox', 'reshape2', 'RColorBrewer',
 		'extrafont', 'latex2exp', 'Cairo', 'gridExtra', 'cowplot'
 		)
 	)
@@ -25,17 +26,17 @@ source(paste0(pathGit, 'R/functions/bayesplot_helpers.R'))
 ###############################################################
 # summarize
 load(paste0(pathResults, 'sobOpp_model1a_1_newp5Var.rda'))
-oppMod = mod
+oppMod <- mod
 load(paste0(pathResults, 'sobState_model1a_1_newp5Var.rda'))
-stateMod = mod
+stateMod <- mod
 
 # vars
-varsRaw = unique(
+varsRaw <- unique(
 	c( rownames(fixef(oppMod)), rownames(fixef(stateMod)) ) )
-vars = unique(gsub('\\[[1-9]\\]','',varsRaw))
-varKey = data.frame(
+vars <- unique(gsub('\\[[1-9]\\]','',varsRaw))
+varKey <- data.frame(
 	dirty = vars, stringsAsFactors = FALSE )
-varKey$clean = c(
+varKey$clean <- c(
 	'Intercept',
 	'ICC Ratification',
 	'Civil War$_{t-1}$',
@@ -47,23 +48,23 @@ varKey$clean = c(
 	'P5 Min. Ideal Pt.$_{t-1}$',
 	'Cumulative\n Govt OSV$_{t-1}$'
 	)
-addCats = function(x,toAdd){
+addCats <- function(x,toAdd){
 	x$dirty=paste0(x$dirty,toAdd);x}
-varKey = rbind(varKey,
+varKey <- rbind(varKey,
 	addCats(varKey,'.1.'),addCats(varKey,'.2.') )
 
 # create model summaries
 ## labels for facets
-gLab = paste0('Global Effects (',c('Opposition','State'),' Model)')
-l1Lab = paste0('No ICC to Prelim Effects (',c('Opposition','State'),' Model)')
-l2Lab = paste0('ICC Prelim to Formal Effects (',c('Opposition','State'),' Model)')
+gLab <- paste0('Global Effects (',c('Opposition','State'),' Model)')
+l1Lab <- paste0('No ICC to Prelim Effects (',c('Opposition','State'),' Model)')
+l2Lab <- paste0('ICC Prelim to Formal Effects (',c('Opposition','State'),' Model)')
 
 # generate pdfs
-ggB = ggplot()+geom_blank(aes(1,1
+ggB <- ggplot()+geom_blank(aes(1,1
                               )) + cowplot::theme_nothing()
 ## rebel model
-rebelSumm = vizWrapper(oppMod, gLab[1], l1Lab[1], l2Lab[1])
-rebelViz = arrangeGrob(
+rebelSumm <- vizWrapper(oppMod, gLab[1], l1Lab[1], l2Lab[1])
+rebelViz <- arrangeGrob(
 	arrangeGrob(ggB,rebelSumm$g,ggB, widths=c(.15,.65,.2)),
 	arrangeGrob(rebelSumm$l1, rebelSumm$l2, ncol=2, widths=c(.55,.45)),
 	nrow=2 )
@@ -72,8 +73,8 @@ ggsave(rebelViz,
 	width=8, height=6, device=cairo_pdf)
 
 ## state model
-stateSumm = vizWrapper(stateMod, gLab[2], l1Lab[2], l2Lab[2])
-stateViz = arrangeGrob(
+stateSumm <- vizWrapper(stateMod, gLab[2], l1Lab[2], l2Lab[2])
+stateViz <- arrangeGrob(
 	arrangeGrob(ggB,stateSumm$g,ggB, widths=c(.15,.65,.2)),
 	arrangeGrob(stateSumm$l1, stateSumm$l2, ncol=2, widths=c(.55,.45)),
 	nrow=2 )
@@ -84,37 +85,96 @@ ggsave(stateViz,
 
 ###############################################################
 # get out stdz tables
-oppTab=stdzTable(oppMod, gLab[1], l1Lab[1], l2Lab[1])
-oppRes = lapply(oppTab, function(tab){ apply(tab, 2, mean) })
+oppTab<-stdzTable(oppMod, gLab[1], l1Lab[1], l2Lab[1])
+oppRes <- lapply(oppTab, function(tab){ apply(tab, 2, mean) })
 # round(cbind(unlist(oppRes)), 2)
-stateTab=stdzTable(stateMod, gLab[2], l1Lab[2], l2Lab[2])
-stateRes = lapply(stateTab, function(tab){ apply(tab, 2, mean) })
+stateTab<-stdzTable(stateMod, gLab[2], l1Lab[2], l2Lab[2])
+stateRes <- lapply(stateTab, function(tab){ apply(tab, 2, mean) })
 # round(cbind(unlist(stateRes)), 2)
 ###############################################################
 
 ###############################################################
 # perf analysis
-x = predict(oppMod)
-data = oppMod$data
-y = data$icclevel_opp_3
-predDF = data.frame(cbind(x, y), stringsAsFactors=FALSE)
-predDF$guess = apply(predDF[,1:3], 1, function(x){
-	which(x==max(x)) })
+getPerfStats <- function(mod, lab){
 
-# compare with polr
-dv = as.character(oppMod$formula$formula)[2]
-ivs = as.character(oppMod$formula$formula)[3]
-ivs = gsub(')','',gsub('cs(','',ivs,fixed=TRUE),fixed=TRUE)
-polrForm = formula( paste0(dv, '~', ivs) )
-data[,dv] = factor(
-	data[,dv],
-	ordered=TRUE,
-	levels=sort(unique(data[,dv]))
+	# extract predictions
+	x <- predict(mod)
+	data <- mod$data
+	y <- data[,1]
+	predDF <- data.frame(cbind(x, y), stringsAsFactors=FALSE)
+	# pick outcome based on max prob assignment
+	predDF$guess <- apply(predDF[,1:3], 1, function(x){
+		which(x==max(x)) })
+
+	## compare with polr
+	# get form for polr from mod
+	dv <- as.character(mod$formula$formula)[2]
+	ivs <- as.character(mod$formula$formula)[3]
+	ivs <- gsub(')','',gsub('cs(','',ivs,fixed=TRUE),fixed=TRUE)
+	polrForm <- formula( paste0(dv, '~', ivs) )
+
+	# make sure dv is structured correctly
+	data[,dv] = factor(
+		data[,dv],
+		ordered=TRUE,
+		levels=sort(unique(data[,dv]))
+	)
+
+	# run polr
+	base <- polr(polrForm, data=data, Hess=TRUE)
+	# extract preds
+	predDF$polrPreds = predict(base)
+
+	# get out perf stats
+	out <- rbind(
+		cbind(
+			tadaa_ord(predDF$y, predDF$guess)$body[,c('value','col_name')],
+			lab=lab, model='Sequential Model' ),
+			cbind(
+				tadaa_ord(predDF$y, predDF$polrPreds)$body[,c('value','col_name')],
+				lab=lab, model='Ordinal Model' )
+	)
+
+	return(out) }
+
+# org
+perfData <- rbind(
+	getPerfStats(oppMod, lab='Opposition-Focused'),
+	getPerfStats(stateMod, lab='State-Focused')
 )
-base = polr(polrForm, data=data, Hess=TRUE)
-predDF$polrPreds = predict(base)
 
-ord_somers_d(predDF$y, predDF$guess)
-ord_somers_d(predDF$y, predDF$polrPreds)
-tadaa_ord(predDF$y, predDF$guess)
+# fix up some labels
+perfVarKey <- data.frame(dirty=unique(perfData$col_name))
+perfVarKey$clean <- c(
+	'Goodman-Kruskal\nGamma',
+	"Somers' D",
+	rep(NA,2),
+	"Kendall's Tau-a",
+	rep(NA,2) )
+
+# merge cleaned labels and subset
+perfData$varName <- perfVarKey$clean[match(perfVarKey$dirty, perfData$col_name)]
+perfData <- perfData[!is.na(perfData$varName),]
+
+# ordering of data
+perfData$lab <- factor(perfData$lab,
+	levels=c('State-Focused', 'Opposition-Focused'))
+perfData$model <- factor(perfData$model,
+		levels=c('Sequential Model', 'Ordinal Model'))
+
+# plot
+cols <- brewer.pal(3, 'RdBu')[c(1,3)]
+ggplot(perfData, aes(x=varName, y=value, color=model)) +
+	geom_point(position = position_dodge(width = 0.5)) +
+	geom_linerange(aes(ymin=0, ymax=value), position = position_dodge(width = 0.5)) +
+	scale_color_manual(values=rev(cols)) +
+	# coord_flip() +
+	facet_wrap(~lab) +
+	labs(x='', y='') +
+	theme(
+		axis.ticks=element_blank(),
+		panel.border=element_blank(),
+		legend.title=element_blank(),
+		legend.position='top'
+	)
 ###############################################################
