9bec4c30a6ad8743df6e51fc0c04ee74fc903f1a
s7minhas <s7.minhas@gmail.com>
2020-03-10 03:43:44 -0400

    fixing notation re rand effs

--- R/analysis/posteriorViz.R
+++ R/analysis/posteriorViz.R
@@ -2,7 +2,7 @@
 if(Sys.info()['user'] %in% c('s7m', 'janus829')){
 	source('~/Research/icc/R/setup.R') }
 
-if(Sys.info()['user'] %in% c('herme','Owner')){
+if(Sys.info()['user'] %in% c('herme','Owner', 'S7M')){
 	user <- Sys.info()['user']
 	baseDir <- paste0('C:/Users/',Sys.info()['user'],'/')
 	source( paste0(baseDir,'Research/icc/R/setup.R') )
