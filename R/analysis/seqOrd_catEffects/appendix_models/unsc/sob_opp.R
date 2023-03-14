708ba241f8f5ead9907d1e6ab29d40527a5d6d45
sminhas <s7.minhas@gmail.com>
2023-03-14 19:27:13 -0400

    finish up appendix items

---
+++ R/analysis/seqOrd_catEffects/appendix_models/unsc/sob_opp.R
@@ -0,0 +1,78 @@
+###############################################################
+source('~/Research/icc/R/setup.R')
+loadPkg(c('sbgcop','brms', 'future'))
+###############################################################
+
+###############################################################
+load(paste0(pathData, 'mergedData_yrly_ongoing.rda.rda'))
+
+## prelim state
+sobOppVars = c(
+	'icc_rat','lag1_civilwar','lag1_polity2',
+	'lag1_gdpCapLog','africa',
+	'lag1_v2juncind',
+	'lag1_osv_rebel_cumul',
+	# p5 vars:
+	'lag1_p5_absidealdiffMin' )
+
+# var transformations
+data$lag1_osv_rebel_cumul = log(data$lag1_osv_rebel_cumul+1)
+data$lag1_osv_rebel_cumul[is.na(data$lag1_osv_rebel_cumul)] = 0
+###############################################################
+
+###############################################################
+# add usnc var for robustness check
+cunsc = c('SUDAN', 'LIBYAN ARAB JAMAHIRIYA')
+
+# create unsc dummy
+data$unsc = 0
+
+# sudan referred by unsc in 2005
+data$unsc[data$cname==cunsc[1] & data$year==2005] = 1
+
+# drop unsc referral case year
+data = data[data$unsc==0,]
+###############################################################
+
+###############################################################
+# org data
+data = data[,c(
+	'ccode','cname','year',
+	'icclevel_opp_3',
+	sobOppVars )]
+
+# listwise deletion
+frame = na.omit(data)
+
+# formatting
+frame$icclevel_opp_3 = as.integer(frame$icclevel_opp_3 + 1)
+frame$ccode = as.integer(frame$ccode)
+###############################################################
+
+###############################################################
+# create p5 variable (2, 365, 220, 710, 200)
+frame$p5 = ifelse(
+	frame$ccode %in% c(2, 365, 220, 710, 200), 0, 1 )
+
+# modify p5 var to be zero if p5 country
+frame$lag1_p5_absidealdiffMin = frame$p5*frame$lag1_p5_absidealdiffMin
+###############################################################
+
+###############################################################
+# category specific effects
+sobOppVars[c(5:8)] = paste0('cs(',sobOppVars[c(5:8)],')')
+
+# run model
+sobOppForm = formula(
+	paste0('icclevel_opp_3 ~ ',
+		paste(sobOppVars, collapse = ' + ') ) )
+mod = brm(
+	formula=sobOppForm,
+	data=frame,
+	family=cratio(link='logit'),
+	cores=4
+	)
+save(
+	mod, 
+	file=paste0(pathResults, 'sobOpp_unsc_fin.rda'))
+###############################################################
